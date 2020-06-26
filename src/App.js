a3c7a8a08851aafe9e32721dee850e3faeae6e83
Mohammed Patel <mopatel1214@gmail.com>
2020-06-25 21:46:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+
+export class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1 style = {{color: "gold", fontSize: "48px", textAlign: 'center'}}> LUC Course Evaluation Web-App </h1>
+      </div>
+    )
+  }
+}
