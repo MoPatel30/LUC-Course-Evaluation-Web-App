4b0f9e3d2d6caa183bdb3ccdaf1d1d7d4b1056f8
Kevin Guillaume <keving3492@gmail.com>
2020-06-30 22:39:50 -0500

    basic template for now

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ export class App extends React.Component{
       <div>
         
         <div className = 'mainHeader'>
-          <img id = 'loyolaLogo' src = {Logo} alt='Loyola Logo' />
+          <a href='./App.js'><img id = 'loyolaLogo' src = {Logo} alt='Loyola Logo' /></a>
           <h1 className= 'siteTitle'> LUC Course Evaluation Web-App </h1>
+          <div id = 'middleLine'></div>
+          <div><a href='./App.js' className = 'courseHead' >Courses</a></div>
+          <div><a href='./App.js' className="profHead">Professors</a></div>
+          <div><a href='./App.js' className="contactHead">Contact</a></div>
         </div>
       
       
